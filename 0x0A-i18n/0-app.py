a8e024e536934eca5c3eb8f57660a961c6b66260
Danny099 <dalmariv01@gmail.com>
2021-08-29 22:30:38 -0500

    add projects

---
+++ 0x0A-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+App FLASK
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def home():
+    """
+    home route
+    return: template
+    """
+    return render_template('0-index.html')
